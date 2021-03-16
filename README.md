7b49eddd09d14ce44adc7373ff033860f574d22e
Louis Maddox <louismmx@gmail.com>
2021-03-16 23:47:57 +0000

    Broadcast class

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ![](https://raw.githubusercontent.com/lmmx/beeb/master/beeb_logo.png)
 
-A modern interface to the BBC Sounds radio catalogue
+A modern interface to the BBC Sounds radio catalogue.
 
 ## Usage
 
-- Create a schedule for BBC radio 4 dated today:
+- Create a schedule for BBC R4, defaulting to today's date:
 
 ```py
 from beeb.nav import Schedule
@@ -16,3 +16,11 @@ Schedule.from_channel_name("r4")
 ```
 Schedule for BBC Radio 4 on 2021-03-16
 ```
+
+## Description
+
+_beeb_ centres around the `Schedule`, which is not limited to a single day's listings:
+
+- ≥1 channels (national, local, regional; indicate by ID or name)
+- ≥1 dates (only today by default, or past N days, or from particular date to another date)
+- ≥1 times of day ('early', 'morning', 'afternoon', 'evening', 'late')
--- src/beeb/nav/sched/schedule.py
+++ src/beeb/nav/sched/schedule.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 from ..channel_ids import ChannelPicker
 from ...time import parse_abs_from_rel_date, cal_path
+from datetime import datetime
 
 __all__ = ["Schedule"]
 
@@ -12,7 +13,12 @@ class Schedule:
     ID from a string (the shortname given in `beeb.nav.channel_ids`).
 
     The contents of a Schedule is the schedule listings for a single day,
-    and the date can be specified as a datetime object `date`.
+    and the date can be specified as a datetime object `date`. The
+    `base_url` gives the current day's schedule, and adding date subpaths
+    onto it gives a calendar view (for just the year or year and month),
+    which are only useful for indicating the furthest ahead available listings,
+    since we can generate the calendar offline in Python,
+    or individual listings (for a full date).
     """
     common_url_prefix = "https://www.bbc.co.uk/schedules/"
 
@@ -27,7 +33,7 @@ class Schedule:
             self.channel
         except Exception as e:
             raise e # channel ID is invalid, don't accept
-        self.url = f"{self.common_url_prefix}{self.channel_id}"
+        self.base_url = f"{self.common_url_prefix}{self.channel_id}"
         self.parse_schedule()
 
     @classmethod
@@ -39,10 +45,38 @@ class Schedule:
         if self.date is None:
             self.date = parse_abs_from_rel_date()
         ymd_path = "/".join(cal_path(self.date, as_tuple=True)) if self.date else None
-        sched_url = f"{self.url}{'/' + ymd_path if self.date else ''}"
-        r = requests.get(self.url)
+        sched_url = f"{self.base_url}{'/' + ymd_path if self.date else ''}"
+        r = requests.get(sched_url)
         r.raise_for_status()
         self.soup = BS(r.content.decode(), features="html5lib")
+        self.broadcasts = [
+            Broadcast.from_soup(b)
+            for b in self.soup.select(".broadcast")
+        ]
 
     def __repr__(self):
         return f"Schedule for {self.channel.title} on {self.date}"
+
+class Broadcast:
+    def __init__(self, dt, pid, title, subtitle):
+        self.time = dt
+        self.pid = pid
+        self.title = title
+        self.subtitle = subtitle
+
+    @classmethod
+    def from_soup(cls, bsoup):
+        """
+        Parse a `div.broadcast` HTML tag in BeautifulSoup.
+        """
+        pid = bsoup.select_one("*[data-pid]")
+        title = bsoup.select_one(".programme__titles .programme__title")
+        subtitle = bsoup.select_one(".programme__titles .programme__subtitle")
+        text_tags = title, subtitle
+        dt = bsoup.select_one("h3.broadcast__time[content]")
+        if not all([pid, dt, *text_tags]):
+            raise ValueError(f"Missing one or more of: {pid=} {title=} {subtitle=}")
+        title, subtitle = [x.text for x in text_tags]
+        pid = pid.attrs["data-pid"]
+        dt = datetime.fromisoformat(dt.attrs["content"])
+        return cls(dt, pid, title, subtitle)
