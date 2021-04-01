26e5dc3c4ff9590d3518c7015423bbc795606f6d
Louis Maddox <louismmx@gmail.com>
2021-04-01 22:41:45 +0100

    Fix tests

--- src/beeb/share/time/tests/test_cal_utils.py
+++ src/beeb/share/time/tests/test_cal_utils.py
@@ -1,41 +1,36 @@
 import pytest
 
-from beeb.share.time.cal_utils import cal_y, cal_m, cal_d, cal_path
+from beeb.share.time.cal_utils import cal_y, cal_m, cal_d, cal_path, cal_date
 from beeb.share.time.cal_utils import y_shift, m_shift, d_shift, cal_shift
 from beeb.share.time.cal_utils import parse_abs_from_rel_date, parse_date_range
 
-@pytest.fixture
-def its_the_nineties_baby():
-    return cal_date(1999, 1, 20)
 
-@pytest.fixture
-def april_fools():
-    return cal_date(2021, 4, 1)
+@pytest.mark.parametrize("y,expected", [(1999, "1999"), (2021, "2021")])
+def test_cal_y_full(y, expected):
+    y_str = cal_y(date=cal_date(y, 1, 15), full_y=True)
+    assert y_str == expected
 
-def check_alnum(alnum_str):
-    return s.isalnum() and not s.isnumeric() and not s.isalpha()
 
-def check_n_digits(digit_str, n=4, alphanumeric=False):
-    is_num = check_alnum(digit_str) if alphanumeric else digit_str.isnumeric()
-    return len(digit_str) == n and digit_str.isnumeric()
+@pytest.mark.parametrize("y,expected", [(1999, "99"), (2021, "21")])
+def test_cal_y_abbrev(y, expected):
+    y_str = cal_y(date=cal_date(y, 1, 15), full_y=False)
+    assert y_str == expected
 
-def test_cal_y_full(april_fools):
-    y_str = cal_y(date=april_fools, full_y=True)
-    assert y_str == "2021"
 
-def test_cal_y_abbrev():
-    "Post-2020, year will never start with '20'. Should be 4 digits."
-    y_str = cal_y(full_y=False)
-    assert not y_str.startswith("20") and check_n_digits(y_str, n=2)
+@pytest.mark.parametrize(
+    "m,zf,expected",
+    [(1, True, "01"), (1, False, "1"), (12, True, "12"), (12, False, "12")],
+)
+def test_cal_m(m, zf, expected):
+    m_str = cal_m(date=cal_date(2021, m, 15), zf=zf)
+    assert m_str == expected
 
-def test_cal_m():
-    "Just check month is numeric string e.g. '01'."
-    m_str = cal_m()
-    assert check_n_digits(m_str, n=4)
 
-def test_cal_m_named():
+@pytest.mark.parametrize(
+    "m,zf,expected",
+    [(1, True, "01jan"), (1, False, "1jan"), (12, True, "12dec"), (12, False, "12dec")],
+)
+def test_cal_m_named(m, zf, expected):
     "Just length check to compare '01' with '01jan' (actual date will vary)"
-    m_str = cal_m(with_name=True)
-    assert check_n_digits(m_str, n=5, alphanumeric=True)
-
-
+    m_str = cal_m(date=cal_date(2021, m, 15), zf=zf, with_name=True)
+    assert m_str == expected
